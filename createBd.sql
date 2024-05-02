f693f74468749786e9d117f31ec7a08513aeb6ce
pablo <p.mendez.vazquez@udc.es>
2024-05-02 13:26:19 +0200

    Script CreateBd Created

---
+++ createBd.sql
@@ -0,0 +1,29 @@
+CREATE TABLE Ubicacion (
+    id INTEGER PRIMARY KEY,
+    dirección VARCHAR(50),
+    provincia VARCHAR(50),
+    códigoPostal VARCHAR(5),
+    ciudad VARCHAR(50)
+);
+
+CREATE TABLE Laboratorio (
+    id INTEGER PRIMARY KEY,
+    nombre VARCHAR(50),
+    especialidad VARCHAR(50),
+    teléfono VARCHAR(9),
+    capacidad INTEGER,
+    idUbicación INT NOT NULL REFERENCES Ubicacion(id)
+);
+
+CREATE TABLE Trabajadores (
+    id INTEGER PRIMARY KEY,
+    DNI VARCHAR(9) UNIQUE,
+    nombre VARCHAR(50),
+    apellido1 VARCHAR(50),
+    apellido2 VARCHAR(50),
+    fechaNacimiento DATE,
+    fechaAlta DATE,
+    puesto VARCHAR(50),
+    idLaboratorio INT NOT NULL REFERENCES Laboratorio(id)
+);
+
